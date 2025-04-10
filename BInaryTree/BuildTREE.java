d3dc4fb22e0352d6c1caa7d61524825a75d8db94
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-04-10 14:31:00 +0530

    Height of tree

--- BInaryTree/BuildTREE.java
+++ BInaryTree/BuildTREE.java
@@ -1,30 +1,27 @@
-public class PreOrderBuild {
+public class BuildTree {
     static class Node{
         int data;
         Node left;
         Node right;
-    //constructor 
+
+        //constructor
         Node(int data){
             this.data=data;
             this.left=null;
             this.right=null;
-
         }
     }
-    static class BinaryTree {
-        static int idx = -1;
-        public static Node buildTree(int nodes[]){
+    static class BinaryTree{
+        static int idx =-1;
+        public static Node buildTree(int nodes []){
             idx++;
-            if(nodes[idx]== -1) return null;
+            if(nodes[idx]==-1)return null;
             Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right =buildTree(nodes);
-
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
             return newNode;
         }
-
     }
-
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
@@ -32,3 +29,5 @@ public class PreOrderBuild {
         System.out.println(root.data);
     }
 }
+
+
