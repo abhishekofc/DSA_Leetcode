35b840611e2ba93e6a88c602782b6301909082b1
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-07-21 13:43:34 +0530

    Content Updated

---
+++ Array/Logic Building/MoveZeroes.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+       int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        Solution sol = new Solution();
+        sol.moveZeroes(nums);
+
+        System.out.print("Array after moving zeroes: ");
+        System.out.println(Arrays.toString(nums));
+    }
+}
