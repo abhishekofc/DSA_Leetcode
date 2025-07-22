53e9418499c4086f39e8f9d7badbb634a3636b19
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-07-22 22:15:04 +0530

    Updated

---
+++ Dynamic Programming/7 Important Concept/Fibonachi/Climbing Stairs/Memoisation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Memoisation{
+    public static int fun(int n , int dp[]){
+        if(n == 0)return 1;
+        if(n<0) return 0 ;
+        // Already calculated
+        if(dp[n] != -1) return dp[n]; 
+        dp[n] = fun(n-1,dp) + fun(n-2,dp);
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp , -1);
+        System.out.println(fun(n,dp));
+    }
+}
