f1885f95a177cb30702e8eea9ae6feaaf9f750c6
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-08-28 13:08:14 +0530

    Basic Maths

---
+++ Basic_Math/CountOddInNum.java
@@ -0,0 +1,22 @@
+
+
+public class CountOddInNum {
+    public static int CountNumDigit(int n){
+        int cnt=0;
+        while(n>0){
+            int last_digit = n%10;
+            if(last_digit%2!=0){
+                cnt++;
+            }
+            n=n/10;
+        }
+        return cnt;
+    }
+    public static void main(String[] args){
+        //Scanner sc = new Scanner(System.in);
+        int n = 25;
+
+        int result = CountNumDigit(n);
+        System.out.println(result);
+    }
+}
