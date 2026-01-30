4caa4045e1999452456d68fb9963aa654ecad418
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2026-01-31 02:38:56 +0530

    Recursion

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Factorial{
+
+    /* 
+    Method 1 - Using Loop
+    public static int fact(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }*/
     public static int fact(int n){
         // Base Case 
         if(n== 0) return 1;
