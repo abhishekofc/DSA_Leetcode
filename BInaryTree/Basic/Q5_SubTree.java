d6fb0e721121a180b757bcb1002e07f0093de13c
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2026-02-24 00:27:16 +0530

    BinaryTree

--- BInaryTree/Basic/Q5_SubTree.java
+++ BInaryTree/Basic/Q5_SubTree.java
@@ -7,12 +7,70 @@ The tree tree could also be considered as a subtree of itself.
 Example 1:
 Input: root = [3,4,5,1,2], subRoot = [4,1,2]
 Output: true
-Example 2:
-Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
-Output: false   
-Constraints:
-The number of nodes in the root tree is in the range [1, 2000].
-The number of nodes in the subRoot tree is in the range [1, 1000].
--10^4 <= root.val <= 10^4   
--10^4 <= subRoot.val <= 10^4
+
+Hint 
+Check for non - identical trees 
+1. node.data != subRoot.data => return false
+2. node = null || subRoot = null => return false
+3. left subtree => Identical or not ?
+4. right subtree => Identical or not ?
+
  */
+
+public class Q5_SubTree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isIdentical(Node root, Node subRoot){
+        if(root == null && subRoot == null) return true;
+        else if(root == null || subRoot == null || root.data != subRoot.data) return false;
+
+        // check for left and right subtree
+        if(!isIdentical(root.left, subRoot.left) || !isIdentical(root.right, subRoot.right)) return false;
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot){
+        if(root == null) return false;
+        if(root.data == subRoot.data){
+            if(isIdentical(root, subRoot)){
+                return true;
+            }
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot); 
+    }
+
+    public static void main(String[] args) {
+            /*
+                      1
+                    /  \
+                   2     3
+                  / \    / \
+                 4  5    6  7
+             */
+            Node root = new Node(1);
+            root.left = new Node(2);
+            root.right= new Node(3);
+            root.left.left=new Node(4);
+            root.left.right=new Node(5);
+            root.right.left = new Node(6);
+            root.right.right = new Node(7);
+            /**
+                      2
+                    /  \
+                   4     5
+            **/
+            Node subRoot = new Node(2);
+            subRoot.left = new Node(4);
+            subRoot.right = new Node(5);
+            System.out.println(isSubtree(root, subRoot));
+        }
+}
